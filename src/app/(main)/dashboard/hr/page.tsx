c0d20867b722225ce114c7ff2016f9f366bf741d
leonidas garcia <leo.garcia.dev.ts@gmail.com>
2026-05-12 10:43:04 -0500

    feat(dashboard): add HR and Manager dashboard page components

---
+++ src/app/(main)/dashboard/hr/page.tsx
@@ -0,0 +1,5 @@
+import { DashboardHRPageContent } from "@/features/dashboard/components/page/DashboardHRPageContent";
+
+export default function DashboardHRPage() {
+  return <DashboardHRPageContent />;
+}
