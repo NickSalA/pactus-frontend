94bff141fdbbe3256acb121d3e3d85d1362d2c95
leonidas garcia <leo.garcia.dev.ts@gmail.com>
2026-05-30 17:49:20 -0500

    feat(api): align API types with OpenAPI specification

--- src/types/api/apiConversation/apiConversationMessage.ts
+++ src/types/api/apiConversation/apiConversationMessage.ts
@@ -1,5 +1,8 @@
+import type { ApiChartData } from '../apiChat/apiChartData';
+
 export interface ApiConversationMessage {
   role: 'user' | 'bot';
   content: string;
   timestamp: string;
+  chart?: ApiChartData;
 }
